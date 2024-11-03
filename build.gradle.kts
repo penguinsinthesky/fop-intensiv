e4b6ada47ee402124f80f7d5cfaa5004e61d06ed
penguins_can_fly <gitlab@sebastianclamer.de>
2024-11-03 08:30:48 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+    application
+}
+
+group = "de.sebastianclamer"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation("org.tudalgo:fopbot:0.8.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass = "intensiv.Main"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/intensiv/Main.java
@@ -0,0 +1,12 @@
+package intensiv;
+
+import fopbot.World;
+
+public class Main {
+
+    public static void main(String[] args) {
+        World.setSize(7, 7);
+        World.setVisible(true);
+    }
+
+}
