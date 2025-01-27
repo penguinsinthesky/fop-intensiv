b35612a3607993494f939af8f91c3480f01bd1ea
penguins_can_fly <gitlab@sebastianclamer.de>
2025-01-27 16:29:54 +0100

    Create task about streams

---
+++ src/main/java/intensiv/I12.java
@@ -0,0 +1,22 @@
+package intensiv;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class I12 {
+
+    public static void filterByLength() {
+        List<String> list = List.of("Hello", "world", "!");
+
+        // TODO I12 a)
+
+    }
+
+    public static void minElement() {
+        int[] someNumbers = {3, 2, 9, 5, 4, 8};
+
+        // TODO I12 b)
+
+    }
+
+}
